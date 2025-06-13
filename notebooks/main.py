e0c367003bd17236e651662c34aefdfdd8f455d9
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-06-13 19:24:41 +0100

    Add main training script for diabetes MLflow classifier

---
+++ notebooks/main.py
@@ -0,0 +1,40 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path().resolve().parent))
+
+
+import pandas as pd
+import mlflow
+from src.models import train_and_log_logistic_regression, train_and_log_random_forest
+from src.metrics import get_metrics
+from src.preprocessing import create_preprocessor
+from src.config import LOG_REG_PARAMS, RF_PARAMS, EXPERIMENT_NAME, TRACKING_URI, NUMERIC_FEATURES, CATEGORICAL_FEATURES
+from sklearn.model_selection import train_test_split
+
+
+
+def run_training():
+    data_path = "../dataset/diabetes.csv"
+    data = pd.read_csv(data_path)
+    features = [col for col in data.columns][1:9]
+    labels = [col for col in data.columns][9]
+
+    X, y = data[features].values, data[labels].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    preprocessor = create_preprocessor(NUMERIC_FEATURES, CATEGORICAL_FEATURES)
+
+    mlflow.set_tracking_uri(TRACKING_URI)
+    mlflow.set_experiment(EXPERIMENT_NAME)
+    
+    log_model  = train_and_log_logistic_regression(X_train, X_test, y_train, y_test, preprocessor, 
+                                                data_path=data_path, **LOG_REG_PARAMS)
+
+    rf_model = train_and_log_random_forest(X_train, X_test, y_train, y_test, preprocessor, 
+                                        data_path=data_path, **RF_PARAMS)
+    
+    print("Training models completed and models logged to MLflow.")
+
+
